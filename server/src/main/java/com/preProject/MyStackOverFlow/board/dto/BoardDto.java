40f3121daec3c945ebd5fbd40be28853b20341dc
jaeyumn <wkddkgodsla@gmail.com>
2023-04-18 10:25:53 +0900

    boardDto 수정

--- server/src/main/java/com/preProject/MyStackOverFlow/board/dto/BoardDto.java
+++ server/src/main/java/com/preProject/MyStackOverFlow/board/dto/BoardDto.java
@@ -1,11 +1,11 @@
 package com.preProject.MyStackOverFlow.board.dto;
 
 import com.preProject.MyStackOverFlow.board.entity.Board;
-import lombok.Data;
-import lombok.Getter;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
 
 public class BoardDto {
@@ -15,12 +15,12 @@ public class BoardDto {
 
         @Positive
         private long memberId;
-        @NotBlank(message = "제목을 작성해 주세요.")
+        @NotEmpty(message = "제목을 작성해 주세요.")
         @Length(max = 100, message = "제목은 100자 이상 작성할 수 없습니다.")
         private String title;
-        @NotBlank(message = "발생한 문제를 작성해 주세요.")
+        @NotEmpty(message = "발생한 문제를 작성해 주세요.")
         private String content;
-        @NotBlank(message = "문제를 해결하기 위해 시도한 내용을 작성해 주세요.")
+        @NotEmpty(message = "문제를 해결하기 위해 시도한 내용을 작성해 주세요.")
         private String contentTry;
     }
 
@@ -29,25 +29,19 @@ public class BoardDto {
 
         @Positive
         private long boardId;
-        @NotBlank(message = "제목을 작성해 주세요.")
         @Length(max = 100, message = "제목은 100자 이상 작성할 수 없습니다.")
         private String title;
-        @NotBlank(message = "발생한 문제를 작성해 주세요.")
         private String content;
-        @NotBlank(message = "문제를 해결하기 위해 시도한 내용을 작성해 주세요.")
         private String contentTry;
     }
 
-    @Getter
+    @Data
+    @Builder
     public static class Response {
-        @Positive
         private long boardId;
-        @NotBlank(message = "제목을 작성해 주세요.")
         @Length(max = 100, message = "제목은 100자 이상 작성할 수 없습니다.")
         private String title;
-        @NotBlank(message = "발생한 문제를 작성해 주세요.")
         private String content;
-        @NotBlank(message = "문제를 해결하기 위해 시도한 내용을 작성해 주세요.")
         private String contentTry;
         private Board.BoardStatus boardStatus;
     }
