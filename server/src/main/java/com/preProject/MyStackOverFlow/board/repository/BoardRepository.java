337fa5769cac6be38175887e4a31683e4eab6241
jaeyumn <wkddkgodsla@gmail.com>
2023-04-18 17:33:13 +0900

    repository 수정

--- server/src/main/java/com/preProject/MyStackOverFlow/board/repository/BoardRepository.java
+++ server/src/main/java/com/preProject/MyStackOverFlow/board/repository/BoardRepository.java
@@ -1,7 +1,16 @@
 package com.preProject.MyStackOverFlow.board.repository;
 
 import com.preProject.MyStackOverFlow.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Page<Board> findByTitleContaining(String title, Pageable pageable);
+    Page<Board> findByContentContaining(String content, Pageable pageable);
+//    Page<Board> findByUsernameContaining(String username, Pageable pageable);
+//    Page<Board> findByTagContaining
 }
