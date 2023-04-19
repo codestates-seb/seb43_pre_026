d33d1934bbb09da6dabd9f2336d5ac71f86e9e84
ock2023 <dmsdhr2605@gmail.com>
2023-04-19 18:14:24 +0900

    기본app

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,5 @@
-import Home from './pages/Home';
-import Questions from './pages/Questions';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UserEdit from './pages/UserEdit';
-
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/questions" element={<Questions />} />
-          <Route path="/useredit" element={<UserEdit />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- client/src/components/HomeList.js
+++ client/src/components/HomeList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HomeItem from './HomeItem';
-import styled from 'styled-components';
 
 const HomeList = () => {
   return (
