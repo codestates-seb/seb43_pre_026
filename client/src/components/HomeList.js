e2637792a81d6d70b46ad6cb0e08520de5fb0a3c
ock2023 <dmsdhr2605@gmail.com>
2023-04-19 11:33:21 +0900

    메인페이지 UI 1차

---
+++ client/src/components/HomeList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HomeItem from './HomeItem';
+import styled from 'styled-components';
+
+const HomeList = () => {
+  return (
+    <div>
+      <HomeItem />
+      <HomeItem />
+      <HomeItem />
+      <HomeItem />
+      <HomeItem />
+    </div>
+  )
+}
+
+export default HomeList;
