6c5414e3637a4a70471b8cb2123d0b9da516d1bd
taeyoung1012 <dlaxodud12@naver.com>
2023-04-19 18:03:54 +0900

    app.js 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,5 @@
-import Signup from './pages/Signup';
-import Questions from './pages/Questions';
-import { Routes, Route } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
-import Header from './components/Header';
-
-const GlobalStyle = createGlobalStyle`
-  html, body {
-    margin: 0;
-    padding: 0;
-  }
-`;
-
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="/q" element={<Questions />} />
-      </Routes>
-    </>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
