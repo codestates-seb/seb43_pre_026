a053fc6e624dc6d58be47054a429f82a3a5bbc75
SEBFE43GwakMiso <kms30922@naver.com>
2023-04-19 17:38:02 +0900

    1차업로드 test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
-import Login from './pages/Login';
+import Signup from "./pages/Signup";
+import Questions from "./pages/Questions";
+import { Routes, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import Header from "./components/Header";
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 function App() {
   return (
-    <Login />
+    <>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/q" element={<Questions />} />
+      </Routes>
+    </>
   );
 }
 
