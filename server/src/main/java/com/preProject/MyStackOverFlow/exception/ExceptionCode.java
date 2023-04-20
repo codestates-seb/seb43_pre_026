00e0000eada62c3cbcd5e7a65d869a3fe840adf1
CDSeoHyunJun <sjmsjm0302@naver.com>
2023-04-20 14:38:56 +0900

    commit

--- server/src/main/java/com/preProject/MyStackOverFlow/exception/ExceptionCode.java
+++ server/src/main/java/com/preProject/MyStackOverFlow/exception/ExceptionCode.java
@@ -7,7 +7,7 @@ public enum ExceptionCode {
     MEMBER_EXISTS(409, "Member exists"),
     INVALID_MEMBER_STATUS(400, "Invalid member status"),
 
-    NOT_IMPLEMENTATION(501, "Not Implementation");
+    NOT_IMPLEMENTATION(501, "Not Implementation"),
 
     BOARD_NOT_FOUND(404, "Board not found"),
     TAG_NOT_FOUND(404, "Tag not found");
